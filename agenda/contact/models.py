d3b84421f4061b7b3fb610ec9ab3911971806015
CiprianNica <ciprian.nica77@gmail.com>
2022-12-12 22:46:59 +0100

    model initial

--- agenda/contact/models.py
+++ agenda/contact/models.py
@@ -4,7 +4,7 @@ from datetime import date
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=50, blank=False, null=False)
-    last_name = models.CharField(max_length=50, blank=True, null=True)
+    last_name = models.CharField(max_length=50, blank=False, null=False)
     phone = models.CharField(max_length=12,blank=True, null=True)
     movil = models.CharField(max_length=12,blank=False, null=False)
     email = models.EmailField()
