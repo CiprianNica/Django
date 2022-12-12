08d8618041137f4f5ce3b461919baa9eb13fcefe
CiprianNica <ciprian.nica77@gmail.com>
2022-12-12 22:23:00 +0100

    commit nodeseado

--- agenda/contact/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='contact'),
-    path('views/<int:id>', views.views, name='contact_views'),
-    path('edit/<int:id>', views.edit, name='contact_edit'),
-]
