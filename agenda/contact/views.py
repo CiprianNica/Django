6917d3ef967fbe0935d7d93a99cd819702fa3ae7
CiprianNica <ciprian.nica77@gmail.com>
2022-12-12 22:12:49 +0100

    first commit de la practica 'gestor personal'

---
+++ agenda/contact/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
+
+def index(request):
+    #contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(name__contains=request.GET.get('search',''))
+    context = {'contacts':contacts}
+    return render(request, 'contact/index.html', context)
+
+def views(request, id):
+    contact = Contact.objects.get(id=id)
+    context = {'contact' : contact}
+    return render (request, 'contact/detail.html', context)
+
+def edit(request, id):
+    contact = Contact.objects.get(id=id)
+    
+    if request.method == 'GET':
+        form = ContactForm(instance=contact)
+        context = {
+            'form': form,
+            'id': id
+            }
+        return render(request, 'contact/create.html', context)
+    
+    if request.method == 'POST':
+        form = ContactForm(request.POST, instance=contact)
+        form.save()
+        context = {
+            'form': form,
+            'id': id,
+        }
+        messages.success(request, 'Contacto actualizado.')
+        return render(request, 'contact/create.html', context)
