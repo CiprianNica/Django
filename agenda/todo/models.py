d6eae8323fe0056e16254100e0b3f5e950d7be20
CiprianNica <ciprian.nica77@gmail.com>
2022-12-19 22:01:48 +0100

    model todo

--- agenda/todo/models.py
+++ agenda/todo/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 from datetime import date
 
-# Create your models here.
 class Todo(models.Model):
-    title = models.CharField(max_length=100, null=False, blank=False)
-    description = models.TextField(blank=True, null=True)
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=False, null=False)
     date = models.DateField(default=date.today)
-    estimated_end = models.DateField(blank=True, null=True)
-    priority = models.IntegerField(default=3)
+    estimated_end = models.DateField(blank=False, null=False)
+    priority =models.IntegerField(default=3)
     
     def __str__(self):
         return self.title
