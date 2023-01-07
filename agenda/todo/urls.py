550f0002f4e3fe1c8d7a5b1555aa147e5bf86d26
CiprianNica <ciprian.nica77@gmail.com>
2023-01-07 21:50:28 +0100

    crud tareas

--- agenda/todo/urls.py
+++ agenda/todo/urls.py
@@ -3,4 +3,8 @@ from todo import views
 
 urlpatterns = [
     path('', views.index, name='todo'),
+    path('create/', views.create, name='todo_create'),
+    path('views/<int:id>', views.view, name='todo_view'),
+    path('edit/<int:id>', views.edit, name= 'todo_edit'),
+    path('delete/<int:id>', views.delete, name='todo_delete'),
 ]
--- agenda/todo/views.py
+++ agenda/todo/views.py
@@ -1,5 +1,71 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Todo
+from .forms import TodoForm
+from django.contrib import messages
 
 def index(request):
-    return render(request, 'todo/index.html', {})
+    todos = Todo.objects.filter(title__contains = request.GET.get('search', ''))
+    context = {
+        'todos':todos
+    }
+    return render(request, 'todo/index.html', context)
+
+def create(request):
+    if request.method == 'GET':
+        form = TodoForm()
+        context = {
+            'form':form
+        }
+        return render(request, 'todo/create.html', context)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context = {
+            'form':form
+        }
+        return render(request, 'todo/create.html', context)
+
+def view(request, id):
+    todo = Todo.objects.get(id=id)
+    context = {
+        'todo':todo
+    }
+    return render(request, 'todo/detail.html', context)
+
+def edit(request, id):
+    todo = Todo.objects.get(id=id)
+    
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        context = {
+            'form': form,
+            'id': id
+        }
+        return render(request, 'todo/edit.html', context)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        form.save()
+        context = {
+            'form':form,
+            'id':id
+        }
+        messages.success(request, 'Profile details updated.')
+        return render(request, 'todo/edit.html', context)
+        
+def delete(request, id):
+    todo = Todo.objects.get(id=id)
+    if request.method == 'GET':
+        form = TodoForm(instance=todo)
+        context = {
+            'form':form,
+            'id':id
+        }
+        messages.warning(request, 'Quieres borrar la tarea ?')
+        return render(request, 'todo/delete.html', context)
+    todo.delete()
+    if request.method == 'POST':
+        messages.error(request, 'Deleted')
+        return redirect('todo')
